3d7587b330e6667014527b5b2d5deb4bd3a96030
Miguel Ochoa Gallegos <miguel.ochoa.gallegos@outlook.com>
2019-04-24 21:51:53 -0500

    fuentes init spring boot

---
+++ src/main/java/pe/com/friterm/portalWeb/PortalWebApplication.java
@@ -0,0 +1,13 @@
+package pe.com.friterm.portalWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalWebApplication.class, args);
+	}
+
+}
